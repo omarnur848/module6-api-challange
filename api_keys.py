3f7193735e524f307a2675f923c626e3be82e873
User <user@users-air.mynetworksettings.com>
2024-11-07 11:51:15 -0500

    final commit for submission

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "9240d6260c75639fede656c1395d1a8e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "93efec8d68094cf3a732783cd01d7928"
